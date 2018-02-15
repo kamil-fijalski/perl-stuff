b33f49da2c2ecf25d37c13b756b703f5f8538c42
Kamil Fijalski <kamil.fijalski@outlook.com>
2018-02-15 12:45:10 +0100

    Add files via upload

---
+++ converter.pl
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+print("Choose your number in decimal system: ");
+chomp(my $num = <>);
+print("Great. You choose $num\n");
+my $result = Convert($num);
+print("In binary system $num is $result");
+
+sub Convert{
+    my ($number) = @_;
+    my $binary = '';
+
+    while($number > 1) {
+        if ($number%2 == 0) {
+            $binary = $binary . '0';}   
+        else {
+        $binary = $binary . '1';}
+
+        $number = int($number / 2);
+    }
+
+    if ($number == 1) {
+        $binary = $binary . '1';}
+
+    return reverse($binary);
+}
